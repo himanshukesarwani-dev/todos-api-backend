cd3d050996ca80220b9a54c91752d7a4bdd46145
HIMANSHUKESARWA\himanshukesarwani <himanshu.k@codecraft.co.in>
2023-10-27 18:42:19 +0530

    feat: added todoSchema

--- dist/todoSchema.js
+++ dist/todoSchema.js
@@ -2,14 +2,12 @@
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable @typescript-eslint/semi */
 import { z } from "zod";
-
 const todoSchema = z.object({
-  id: z.number(),
-  title: z.string(),
-  description: z.string(),
-  dueDate: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
+    id: z.number(),
+    title: z.string(),
+    description: z.string(),
+    dueDate: z.string(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
 });
-
 export { todoSchema };
