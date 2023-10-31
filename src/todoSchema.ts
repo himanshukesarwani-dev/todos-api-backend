ce129a7ffcac35d9ee7f69ac73a98441c12a5688
HIMANSHUKESARWA\himanshukesarwani <himanshu.k@codecraft.co.in>
2023-10-31 19:15:05 +0530

    fix: added more specific custom data validation using zod

--- src/todoSchema.ts
+++ src/todoSchema.ts
@@ -2,12 +2,25 @@
 import { z } from "zod";
 
 const todoSchema = z.object({
-  id: z.number(),
-  title: z.string(),
-  description: z.string(),
-  dueDate: z.string(),
-  createdAt: z.string().datetime(),
-  updatedAt: z.string().datetime(),
+  id: z.number().int("Id must be an Integer."),
+  title: z.string().min(2, "Title must be minimum 2 characters long."),
+  description: z
+    .string()
+    .min(5, "Description must be minimum 5 characters long."),
+  dueDate: z
+    .string()
+    .regex(
+      /^\d{4}-\d{2}-\d{2}$/,
+      "Due Date must be in the format 'YYYY-MM-DD'.",
+    ),
+  createdAt: z.string().refine((value) => {
+    const date = new Date(value);
+    return !isNaN(date.getTime());
+  }, "Invalid date format for createdAt."),
+  updatedAt: z.string().refine((value) => {
+    const date = new Date(value);
+    return !isNaN(date.getTime());
+  }, "Invalid date format for updatedAt."),
 });
 
 export { todoSchema };
