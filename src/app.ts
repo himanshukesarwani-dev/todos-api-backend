b13a0c2e1d344315d9b37695f981d49f14f84809
HIMANSHUKESARWA\himanshukesarwani <himanshu.k@codecraft.co.in>
2023-11-09 17:52:39 +0530

    feat: added routeNotFound and errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import todoRouter from "./routes/todoRoutes.js";
 import apiAuthMiddleware from "./middlewares/apiAuthMiddleware.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { routeNotFound } from "./middlewares/routeNotFound.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(apiAuthMiddleware);
 app.use("/api/v1/todos", todoRouter);
+app.use(routeNotFound);
+app.use(errorHandler);
 
 // server
 app.listen(PORT, () => {
