d6373889524aa28c13ed7b6d1ddd3e434545cfee
HIMANSHUKESARWA\himanshukesarwani <himanshu.k@codecraft.co.in>
2023-11-15 12:06:00 +0530

    feat: added route  for updateTodo

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -3,10 +3,11 @@ import {
   createTodo,
   getATodo,
   getAllTodos,
+  updateTodo
 } from "../controllers/todoControllers.js";
 const router = express.Router();
 
 router.route("/").post(createTodo).get(getAllTodos);
-router.route("/:id").get(getATodo);
+router.route("/:id").get(getATodo).put(updateTodo);
 
 export default router;
